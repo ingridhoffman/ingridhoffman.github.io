ac4f42d4ff4fd41e0b66d340b3314a9a35da177e
ingridhoffman <ingridhoffman.pdx@gmail.com>
2020-09-25 20:24:44 -0700

    Updates

---
+++ precache-manifest.88f94cec453686c83bcfb5c2e32af8f8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9985010dae838bc4a1525de5314648e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "812ee94acaf40524ed60",
+    "url": "/static/css/main.0b7d2aef.chunk.css"
+  },
+  {
+    "revision": "563f5129dc8286741577",
+    "url": "/static/js/2.1086f837.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.1086f837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812ee94acaf40524ed60",
+    "url": "/static/js/main.2c137baa.chunk.js"
+  },
+  {
+    "revision": "97ad9e083897a8286841",
+    "url": "/static/js/runtime-main.d7d8550f.js"
+  },
+  {
+    "revision": "f5e9ebcf5ea41b16af925f6ad313955a",
+    "url": "/static/media/noun_Communication_925161.f5e9ebcf.png"
+  },
+  {
+    "revision": "31d5babc2abb84d4b235c49b70403703",
+    "url": "/static/media/noun_Idea_204651.31d5babc.png"
+  },
+  {
+    "revision": "6c5101ceef7480412e13811f414155f3",
+    "url": "/static/media/noun_Planning process_2148913.6c5101ce.png"
+  },
+  {
+    "revision": "749a6033b9cbc866a6da0449f8240886",
+    "url": "/static/media/noun_grow_2134952.749a6033.png"
+  },
+  {
+    "revision": "6094c186ef2f3c727dffb7f6abb87792",
+    "url": "/static/media/noun_learn_355071.6094c186.png"
+  },
+  {
+    "revision": "d36f7c800597d79a65004c4260fe8ade",
+    "url": "/static/media/noun_management_2025303.d36f7c80.png"
+  },
+  {
+    "revision": "ded5669920c235cd1db5d66253080cff",
+    "url": "/static/media/noun_networking_2148898.ded56699.png"
+  },
+  {
+    "revision": "f27562320a51642c3867a572b8568be7",
+    "url": "/static/media/noun_problem_2148912.f2756232.png"
+  }
+]);
